d0ba15ce420160e76cfb915027a0a585d7dca523
thanhnd12 <thanhnd12@vng.com.vn>
2019-06-18 13:58:39 +0700

    clean

--- src/store/configureStore.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod').default; // eslint-disable-line global-require
-} else {
-  module.exports = require('./configureStore.dev').default; // eslint-disable-line global-require
-}
