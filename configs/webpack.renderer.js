8a8b32972b104790253ffac28e59ed00e04208b3
thanhnd12 <thanhnd12@vng.com.vn>
2019-06-18 19:22:40 +0700

    draft

--- configs/webpack.renderer.js
+++ configs/webpack.renderer.js
@@ -1,11 +1,29 @@
+const path = require("path");
+const webpack = require("webpack");
 const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { outDir, isProduction, port } = require("../src/utils/env");
 const baseConfigs = require("./webpack.base");
+const { outDir, isProduction, port, dllDir } = require("../src/utils/env");
 
-module.exports = merge(baseConfigs, {
+const getDllConfig = () => {
+  const manifestPath = path.resolve(dllDir, "./libraries.json");
+  const manifest = require(manifestPath);
+  return merge({
+    entry: ["../dll/libraries.dll.js"],
+    plugins: [
+      new webpack.DllReferencePlugin({
+        context: dllDir,
+        manifest
+      })
+    ]
+  });
+};
+const dllConfigs = !isProduction && getDllConfig();
+
+module.exports = merge(baseConfigs, dllConfigs, {
   mode: process.env.NODE_ENV,
-  entry: "../src/renderer.js",
+  target: "electron-renderer",
+  entry: ["../src/renderer.js"],
   output: {
     path: isProduction ? outDir : undefined,
     filename: "renderer.js"
